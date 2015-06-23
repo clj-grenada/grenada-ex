59482fd01f7314e0106737cc393cc152e3acd076
Richard Möhn <richard.moehn@posteo.de>
2015-06-23 21:09:51 +0900

    Add some corrections and stuff

--- dev/user.clj
+++ dev/user.clj
@@ -12,12 +12,17 @@
             [grenada-lib.mergers :as gr-merge]
             poomoo.transformers))
 
+(clojure.tools.namespace.repl/set-refresh-dirs
+  "src"
+  "dev"
+  "checkouts/grenada-lib/src")
+
 ;; TODO: This has to become a Guten-tag predicate. (RM 2015-06-21)
 (defn def? [m]
   (= (safe-get m :level) :grimoire.things/def))
 
 (def produce-skeleton-graph
-  "Input nodes: dp-metadata"
+  "Input nodes: dp-metadata ext-metadata-file"
   {:stripped
    (fnk* [dp-metadata] (map gr-trans/strip-all))
 
@@ -32,40 +37,57 @@
    (fnk* [with-ext] gr-trans/reorder-for-output)
 
    :export-res
-   (fnk [nicely-sorted]
-     (gr-exp/pprint-fs-flat nicely-sorted "darkestperu-examples.edn"))})
+   (fnk* [nicely-sorted ext-metadata-file] gr-exp/pprint-fs-flat)})
 
 (def produce-jar-graph
   "Input nodes: dp-metadata ext-metadata-file hier-out-dir jar-out-dir
                 jar-coords"
   {:ext-metadata
-   (fnk* [ext-metadata-file] grenada-lib.util/read-metadata)
+   (fnk* [ext-metadata-file] gr/read-metadata)
 
    :postprocessed
-   (fnk* [ext-metadata]
+   (fnk [ext-metadata jar-coords]
+     (println ext-metadata)
      (map (gr-trans/transform-ext :poomoo.ext/ex-raw
-                                   poomoo.transformers/process-raw)))
+                                  (poomoo.transformers/process-raw jar-coords))
+          ext-metadata))
 
    :merged
    (fnk* [dp-metadata postprocessed] gr-merge/simple-merge)
 
    :exported-files
-   (fnk* [:merged :hier-out-dir] gr-exp/fs-hier)
+   (fnk* [merged hier-out-dir] gr-exp/fs-hier)
 
    :jar-export-res
-   (fnk* [:hier-out-dir :jar-out-dir :jar-coords] gr/jar-from-files)})
+   (fnk* [hier-out-dir jar-out-dir jar-coords] gr/jar-from-files)})
+
+(defn get-common-config []
+  {:dp-metadata
+   (gr-clj-src/clj-entity-src
+    ['org.clojure/clojure "1.7.0-RC2"]
+    ['clj-grenada/darkestperu "0.1.0-SNAPSHOT"]
+    {:group "clj-grenada"
+     :name "darkestperu"
+     :version "0.1.0-SNAPSHOT"})
+
+   :ext-metadata-file "darkestperu-examples.edn"})
 
 (comment
 
   (refresh)
   (((graph/eager-compile produce-skeleton-graph)
-    {:dp-metadata
-     (gr-clj-src/clj-entity-src
-       ['org.clojure/clojure "1.7.0-RC2"]
-       ['clj-grenada/darkestperu "0.1.0-SNAPSHOT"]
-       {:group "clj-grenada"
-        :name "darkestperu"
-        :version "0.1.0-SNAPSHOT"})})
+    (get-common-config))
    :export-res)
+
+  (refresh)
+
+  ((graph/lazy-compile produce-jar-graph)
+   (merge (get-common-config)
+          {:hier-out-dir "grenada-data"
+           :jar-out-dir "target/grenada"
+           :jar-coords {:group "org.clojars.rmoehn"
+                        :name "darkestperu-examples"
+                        :version "0.1.0-SNAPSHOT"}}))
+  (def res *1)
 
   )
--- src/poomoo/transformers.clj
+++ src/poomoo/transformers.clj
@@ -1,10 +1,26 @@
-(ns poomoo.transformers)
+(ns poomoo.transformers
+  (:require [clojure.string :as string]
+            [plumbing.core :refer [safe-get]]))
+
+(defn mapv-some [& args]
+  (filter some? (apply mapv args)))
+
+(defn empty-example? [ex]
+  (= ex {:name "" :content ""}))
 
 ;; TODO: (among many other things) Add a specification for example maps. (RM
 ;;       2015-06-21)
-(defn transform-raw-example [m e]
-  {:content}
-  )
+(defn transform-raw-example [jar-coords]
+  (fn [m e]
+    (if (empty-example? e)
+      nil
+      {:content (string/replace (safe-get e :content) #"\n\s*" "\n")
+       :name (safe-get e :name)
+       :coords jar-coords})))
 
-(defn process-raw [v]
-  [:poomoo.ext/examples (mapv transform-raw-example v)])
+(defn process-raw [jar-coords]
+  (fn [v]
+    (let [processed-exs (mapv-some (transform-raw-example jar-coords) v)]
+      (if (empty? processed-exs)
+        nil
+        [:poomoo.ext/examples ]))))
