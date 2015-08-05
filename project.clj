b42a4f2587de0f39631cc1313f1056ad055be185
Richard Möhn <richard.moehn@posteo.de>
2015-08-05 11:17:55 +0900

    Remove jolly stuff
    It has its own library now.

--- project.clj
+++ project.clj
@@ -7,10 +7,11 @@
                  [org.clj-grenada/lib-grenada "0.2.1"]
                  [prismatic/plumbing "0.4.4"]
                  [me.arrdem/guten-tag "0.1.4"]
+                 [org.clj-grenada/jolly "0.1.0-SNAPSHOT"]
                  ;; Remove the (remove … ".git") when bumping.
                  [org.clojure-grimoire/lib-grimoire "0.10.2"]
 
-                 ; dependencies to be scraped
+                 ; dependencies to be scraped – should go in dev profile
                  [clj-grenada/darkestperu "0.1.0-SNAPSHOT"]]
 
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
