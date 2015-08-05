25f194f1f7c59faedda2e21ccaa2f289e8a3bb89
Richard Möhn <richard.moehn@posteo.de>
2015-08-05 11:22:03 +0900

    Add a parser and example text

---
+++ resources/grim-examples.clj
@@ -0,0 +1,28 @@
+;;; These are taken as test input from http://conj.io/contributing.
+
+4
+;; => 4
+
+(inc  [1])
+;; Ouch!
+;; => ClassCastException clojure.lang.PersistentVector cannot be cast to
+;; java.lang.Number
+
+(read)
+;; < (1 2 3)
+;; => (1 2 3)
+(do  (.write *err* "To error!\n")
+     (.write *out* "To out!\n")
+    nil)
+;; >> To error!
+;; > To out!
+;; => nil
+
+(print  (meta #'clojure.core/for))
+;; => {:arglists ([seq-exprs body-expr]),
+;;     :added 1.0,
+;;     :line 4444,
+;;     :column 1,
+;;     :file clojure/core.clj,
+;;     :name for,
+;;     :macro true}
